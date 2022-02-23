155dafb17ae4b399f7a2771737fafab13fbf9485
Thisisyusub <kanan.yusub@gmail.com>
2022-02-23 19:16:57 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  C++ Learn
+//
+//  Created by Kenan on 22.02.22.
+//
+
+/*
+ 1. Anatomy of C++ program (cin, cout, namespace, preprocessor)
+ 2. Using variables, declaring constants
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+//     int x;
+//     cout << "Enter x: ";
+//     cin >> x;
+//
+//    int a,b,c = 0;
+//
+//    bool isOkey = false;
+//    char symbol = '*';
+//
+//    short int gradesInMath = -5;
+//    int moneyInBank = -70000;
+//    long populationChange = -85000;
+//    long long countryGDP_YoY = -70000000000;
+//
+//    unsigned short int numColorsInRainbow = 7;
+//    unsigned int numEggsInBasket = 24;
+//    unsigned long  numCarsInNewYork = 700000;
+//    unsigned long long countryMedicareExpense = 70000000000;
+//
+//
+//    short signedShort = 32767;
+//    cout << "signed short 32767 + 1 = ";
+//    cout << ++signedShort << endl;
+//
+//    float pi = 3.14;
+//    double morePrecisePi = 22.0 / 7;
+//    double newPi = 3.141'592'653'59;
+//
+//    cout << "size of double: " << sizeof(double);
+    
+    
+//    int largeNum = 5000000;
+//    short anotherNum{ largeNum };
+//    int anotherNum2{ largeNum };
+//    float someFloat{ largeNum };
+    
+//    auto coinFlippedHeads = true;
+    
+//    typedef unsigned int STRICTLY_POSITIVE_INTEGER;
+//    STRICTLY_POSITIVE_INTEGER numEggsInBasket = 45;
+    
+    
+//    int someNumber = 012; // octal 12 evaluates to decimal 10
+//    int someNumber2 = 0b1010; // binary 1010 evaluates to decimal 10
+    
+    const double pi = 22.0 / 7;
+    cout << "The value of constant pi is: " << pi << endl;
+    
+    return 0;
+}
+
